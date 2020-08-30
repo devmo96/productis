917b7a072d1fa7bd9dde5e5cdc96b7939a30958b
devmo96 <52043149+devmo96@users.noreply.github.com>
2020-08-29 21:31:43 -0400

    Updates

---
+++ precache-manifest.06a9d251afe649cf33b5e31cebe6e8f0.js
@@ -0,0 +1,458 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11e9ca084404f42b1fb19a4d06e6a2bb",
+    "url": "/production/index.html"
+  },
+  {
+    "revision": "b3a52b620b0fc9656a30",
+    "url": "/production/static/css/2.e8b6266b.chunk.css"
+  },
+  {
+    "revision": "3c734ab1c596a8de7a20",
+    "url": "/production/static/css/main.2bbd2557.chunk.css"
+  },
+  {
+    "revision": "b3a52b620b0fc9656a30",
+    "url": "/production/static/js/2.0b09bc7b.chunk.js"
+  },
+  {
+    "revision": "a51a713c3527a2431a1b1bcb8075d48f",
+    "url": "/production/static/js/2.0b09bc7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c734ab1c596a8de7a20",
+    "url": "/production/static/js/main.ce05fcac.chunk.js"
+  },
+  {
+    "revision": "02920c6d65da682fcc7c",
+    "url": "/production/static/js/runtime-main.1f0cd7df.js"
+  },
+  {
+    "revision": "e7d3be8e24d6cb7d138a6d029f462087",
+    "url": "/production/static/media/1.e7d3be8e.jpg"
+  },
+  {
+    "revision": "d92a869395be70c22fbb95616a3f56a9",
+    "url": "/production/static/media/11in-crystal.d92a8693.mp3"
+  },
+  {
+    "revision": "a0634073a226d4ac09deeeaa8148a16f",
+    "url": "/production/static/media/13in-crystal.a0634073.mp3"
+  },
+  {
+    "revision": "024217ee7cf1c6c82b3b5e45f50ae75d",
+    "url": "/production/static/media/14in-crystal.024217ee.mp3"
+  },
+  {
+    "revision": "0ac47f0ed3c021b69a70dd76afb8320a",
+    "url": "/production/static/media/16in-crystal.0ac47f0e.mp3"
+  },
+  {
+    "revision": "bdb8340ca70251e98eeaa38e1db70291",
+    "url": "/production/static/media/18in-crystal.bdb8340c.mp3"
+  },
+  {
+    "revision": "10397606fcfe5220a68e998438d73cab",
+    "url": "/production/static/media/2.10397606.jpg"
+  },
+  {
+    "revision": "9a718a9796b237bc7372c50b0397a2db",
+    "url": "/production/static/media/3.9a718a97.jpg"
+  },
+  {
+    "revision": "9b64e4514f32863c75dd5daf60117b53",
+    "url": "/production/static/media/4.9b64e451.jpg"
+  },
+  {
+    "revision": "e4ee05b37bfc449e36f4b57f9d114ba6",
+    "url": "/production/static/media/5.e4ee05b3.jpg"
+  },
+  {
+    "revision": "528170dab46e5ae3c9cde7065c5b96fc",
+    "url": "/production/static/media/6.528170da.jpg"
+  },
+  {
+    "revision": "091dae4345ef43a741ea76943f956c0c",
+    "url": "/production/static/media/7.091dae43.jpg"
+  },
+  {
+    "revision": "d35193fa3cbbb280130b781fbe9d45d8",
+    "url": "/production/static/media/9in-crystal.d35193fa.mp3"
+  },
+  {
+    "revision": "e7bf94270eab03b9ca035a64d00681fe",
+    "url": "/production/static/media/air-condition.e7bf9427.mp3"
+  },
+  {
+    "revision": "f568e2be90fbd35f7927e8e5881b17b5",
+    "url": "/production/static/media/airplane.f568e2be.mp3"
+  },
+  {
+    "revision": "c0962782a83ff522b4ea37ced3b522a9",
+    "url": "/production/static/media/arcade-bar.c0962782.mp3"
+  },
+  {
+    "revision": "d7b15768cea3fc69a21b23b1740e8430",
+    "url": "/production/static/media/automn-wind.d7b15768.mp3"
+  },
+  {
+    "revision": "2f4f35e9e8ecae238b7779d89f579f63",
+    "url": "/production/static/media/babbling-creek.2f4f35e9.mp3"
+  },
+  {
+    "revision": "c6fc9038daa4b9f75c8e1570467dc4fe",
+    "url": "/production/static/media/bamboo-windchimes.c6fc9038.mp3"
+  },
+  {
+    "revision": "0d1200e882de08b151fe27cc1c231999",
+    "url": "/production/static/media/beach-fire.0d1200e8.mp3"
+  },
+  {
+    "revision": "23e9a437afa2367ba1c4b909f6f3fc9f",
+    "url": "/production/static/media/beach.23e9a437.mp3"
+  },
+  {
+    "revision": "6ca8da966a4cbddae6017a64e6107ef0",
+    "url": "/production/static/media/boat.6ca8da96.mp3"
+  },
+  {
+    "revision": "f13c3c8168c68a38efaa0c01558a4f66",
+    "url": "/production/static/media/boiling-water.f13c3c81.mp3"
+  },
+  {
+    "revision": "d794b7192031a5c27ad42e84ad27e2f8",
+    "url": "/production/static/media/breathing-wind.d794b719.mp3"
+  },
+  {
+    "revision": "da699fa8ad86924d406333524028961f",
+    "url": "/production/static/media/brown-noise.da699fa8.mp3"
+  },
+  {
+    "revision": "4624f75a5f44a83f9cbd98757d26ef38",
+    "url": "/production/static/media/camp-fire.4624f75a.mp3"
+  },
+  {
+    "revision": "75959a02b810b535033a570303d06fde",
+    "url": "/production/static/media/canary.75959a02.mp3"
+  },
+  {
+    "revision": "d10dc7bff10c6215a7bc4dafe03bd244",
+    "url": "/production/static/media/carnival.d10dc7bf.mp3"
+  },
+  {
+    "revision": "0166d26b97a307eccb574559dff02f70",
+    "url": "/production/static/media/cat-purr.0166d26b.mp3"
+  },
+  {
+    "revision": "16dfeff2dfe9df83b063232e7e395b49",
+    "url": "/production/static/media/cave-river.16dfeff2.mp3"
+  },
+  {
+    "revision": "ee695a2ffa2aeb9919f1b2e8e549627f",
+    "url": "/production/static/media/cave-water.ee695a2f.mp3"
+  },
+  {
+    "revision": "61864bbb6804d6f9cd6983436b6dc3c5",
+    "url": "/production/static/media/city-at-night.61864bbb.mp3"
+  },
+  {
+    "revision": "aebc97ce701cd2fe14da8775c74ceb18",
+    "url": "/production/static/media/city-wind.aebc97ce.mp3"
+  },
+  {
+    "revision": "990961b87ee3a36a00813ede8bc3e067",
+    "url": "/production/static/media/clock.990961b8.mp3"
+  },
+  {
+    "revision": "4973fa7768ce53067c6048564141c113",
+    "url": "/production/static/media/coffee-brew.4973fa77.mp3"
+  },
+  {
+    "revision": "ad5833d46fcc71d594009dd83f06847b",
+    "url": "/production/static/media/coffee-shop.ad5833d4.mp3"
+  },
+  {
+    "revision": "0155c082ad547d82950155da3c93232b",
+    "url": "/production/static/media/crickets.0155c082.mp3"
+  },
+  {
+    "revision": "f7e19d99411212eb467065d7dbdf1a21",
+    "url": "/production/static/media/crowd.f7e19d99.mp3"
+  },
+  {
+    "revision": "f3b57203a7de8f48a359d178f9c9e797",
+    "url": "/production/static/media/desert-wind.f3b57203.mp3"
+  },
+  {
+    "revision": "f04038a10d1e46dfb03d5258ef8c6186",
+    "url": "/production/static/media/dish-washer.f04038a1.mp3"
+  },
+  {
+    "revision": "33eb1eeb0c302a5b6599646ea6ab80b4",
+    "url": "/production/static/media/dog-whining.33eb1eeb.mp3"
+  },
+  {
+    "revision": "9a0c408b0efffaf62daf99117f888ff9",
+    "url": "/production/static/media/dryer.9a0c408b.mp3"
+  },
+  {
+    "revision": "28eaa0c293bc3494ea952a576436697f",
+    "url": "/production/static/media/fan.28eaa0c2.mp3"
+  },
+  {
+    "revision": "10c40884cf8d4c1f94592e37bdbbf369",
+    "url": "/production/static/media/fire.10c40884.mp3"
+  },
+  {
+    "revision": "ed3cccbee84c0dc13fb69966d1e2d8ee",
+    "url": "/production/static/media/fireplace.ed3cccbe.mp3"
+  },
+  {
+    "revision": "fb43a2b591c4e3c074df78f8c71df20e",
+    "url": "/production/static/media/fish-tank.fb43a2b5.mp3"
+  },
+  {
+    "revision": "56223b6c33dd724e11f24ccf8c416ddf",
+    "url": "/production/static/media/flag-flicking.56223b6c.mp3"
+  },
+  {
+    "revision": "42d78c1d1c212537ee6b77b591806ff2",
+    "url": "/production/static/media/flame-movement.42d78c1d.mp3"
+  },
+  {
+    "revision": "57421bf7e4ffe12ff5880cec15861b46",
+    "url": "/production/static/media/food-court.57421bf7.mp3"
+  },
+  {
+    "revision": "94f83df7e54b76683189e192accecb34",
+    "url": "/production/static/media/forest-river.94f83df7.mp3"
+  },
+  {
+    "revision": "f9252f2ba8c8d821a2e53386a2629538",
+    "url": "/production/static/media/forest-wind.f9252f2b.mp3"
+  },
+  {
+    "revision": "4f5661ce3824de7985a30d3ce9ee342b",
+    "url": "/production/static/media/fountain.4f5661ce.mp3"
+  },
+  {
+    "revision": "3959249c68ffa15fbcd1384d6edbf564",
+    "url": "/production/static/media/fridge.3959249c.mp3"
+  },
+  {
+    "revision": "51f066f8392f4e2f1a0539ed3f0fcdb0",
+    "url": "/production/static/media/geese.51f066f8.mp3"
+  },
+  {
+    "revision": "85354395f852aac3cec253dd50074066",
+    "url": "/production/static/media/gym.85354395.mp3"
+  },
+  {
+    "revision": "138680c0e702561e1d7838656da5fa1e",
+    "url": "/production/static/media/hair-dryer.138680c0.mp3"
+  },
+  {
+    "revision": "9da75731df39cd2d0fa1b39cc18d8042",
+    "url": "/production/static/media/heart-beats.9da75731.mp3"
+  },
+  {
+    "revision": "a9514bbbbf7a5b23f170b307b846b1fd",
+    "url": "/production/static/media/heavy-rain.a9514bbb.mp3"
+  },
+  {
+    "revision": "34371d6699c282317d8400913028ec06",
+    "url": "/production/static/media/hudson-river.34371d66.mp3"
+  },
+  {
+    "revision": "5715456ddcda60e9b4e963ec5fcea2d7",
+    "url": "/production/static/media/ice-cube-melting.5715456d.mp3"
+  },
+  {
+    "revision": "1734a6d8c5332e29160874d2d6084a30",
+    "url": "/production/static/media/jungle-birds.1734a6d8.mp3"
+  },
+  {
+    "revision": "df95143ab00b1cb121bc2a59726a3c1f",
+    "url": "/production/static/media/kitchen-sink.df95143a.mp3"
+  },
+  {
+    "revision": "1c9e963d94549be028d0952f03e62132",
+    "url": "/production/static/media/lake.1c9e963d.mp3"
+  },
+  {
+    "revision": "4d12271417dffa761f1b0d3d68a60042",
+    "url": "/production/static/media/light-rain.4d122714.mp3"
+  },
+  {
+    "revision": "c17a390a973b75cf1c7ee77f5dbeec0f",
+    "url": "/production/static/media/magpie.c17a390a.mp3"
+  },
+  {
+    "revision": "a169d784454b6efedef6bb04667f90f7",
+    "url": "/production/static/media/meadow-creek.a169d784.mp3"
+  },
+  {
+    "revision": "a9899a7b61cb694c6910bd92633cd0a2",
+    "url": "/production/static/media/mountain-wind.a9899a7b.mp3"
+  },
+  {
+    "revision": "333a5d17307ef945e0f49587689eaa30",
+    "url": "/production/static/media/outdoor-fountain.333a5d17.mp3"
+  },
+  {
+    "revision": "bfa2ee62e6d1b4994be046bab5171ae0",
+    "url": "/production/static/media/paper-shredder.bfa2ee62.mp3"
+  },
+  {
+    "revision": "1c92b6de3c27d8faad042a37f8fc535f",
+    "url": "/production/static/media/park-ducks.1c92b6de.mp3"
+  },
+  {
+    "revision": "a6186147d0f45833cd7147a3016ae71d",
+    "url": "/production/static/media/park.a6186147.mp3"
+  },
+  {
+    "revision": "32378f4e346454c9d5b0bd6520015caa",
+    "url": "/production/static/media/pink-noise.32378f4e.mp3"
+  },
+  {
+    "revision": "6248d3aed0444900541be89e14e0b171",
+    "url": "/production/static/media/rain-on-car.6248d3ae.mp3"
+  },
+  {
+    "revision": "fd2f3e56b4e4f6aba98950475e2fd767",
+    "url": "/production/static/media/rain-on-lake.fd2f3e56.mp3"
+  },
+  {
+    "revision": "17f2f3be97d6f7403854848a54914a6a",
+    "url": "/production/static/media/rain-on-tin-roof.17f2f3be.mp3"
+  },
+  {
+    "revision": "f35a52723f5eb4fb6555ecadf5ea467f",
+    "url": "/production/static/media/river.f35a5272.mp3"
+  },
+  {
+    "revision": "f1cf65bfc6cc598c8041aa5053bf68f4",
+    "url": "/production/static/media/seagulls.f1cf65bf.mp3"
+  },
+  {
+    "revision": "e7690efbfb4f4342fb1069c0c8ef852b",
+    "url": "/production/static/media/seminar.e7690efb.mp3"
+  },
+  {
+    "revision": "69a356a7b31d3e3492864f10d0782ef2",
+    "url": "/production/static/media/shopping-mall.69a356a7.mp3"
+  },
+  {
+    "revision": "58f5ab55d27d57eaf42a19d1dedfe712",
+    "url": "/production/static/media/shower.58f5ab55.mp3"
+  },
+  {
+    "revision": "5c1bcf4bafea13f1efed948f0deecd29",
+    "url": "/production/static/media/small-campfire.5c1bcf4b.mp3"
+  },
+  {
+    "revision": "6907738c9d0a82a58dd3b5457d2839fe",
+    "url": "/production/static/media/spaceship.6907738c.mp3"
+  },
+  {
+    "revision": "f9723f0b6ffd2f751a5b011957285cdd",
+    "url": "/production/static/media/sparrows.f9723f0b.mp3"
+  },
+  {
+    "revision": "fd223d3732702aba1718f4873c3d9c17",
+    "url": "/production/static/media/starlings.fd223d37.mp3"
+  },
+  {
+    "revision": "d073102bb1c41286d8b6a3f97ab568c4",
+    "url": "/production/static/media/stove-heater.d073102b.mp3"
+  },
+  {
+    "revision": "681f3d7a3f372d44a57bf77f3a7bd119",
+    "url": "/production/static/media/stream.681f3d7a.mp3"
+  },
+  {
+    "revision": "d57b08f0a510b08ea4ca84c8a6869b3f",
+    "url": "/production/static/media/strong-wind.d57b08f0.mp3"
+  },
+  {
+    "revision": "7cd211f2ae8f25ae527fd05b83ba5762",
+    "url": "/production/static/media/submarine-interior.7cd211f2.mp3"
+  },
+  {
+    "revision": "588bafdd95127f29be01f4aea9cf64a0",
+    "url": "/production/static/media/summer-rain.588bafdd.mp3"
+  },
+  {
+    "revision": "9621b3a05d1d2e5da24d3132e1968461",
+    "url": "/production/static/media/train-interior.9621b3a0.mp3"
+  },
+  {
+    "revision": "710c0758815ce753021aa0a84cad74d7",
+    "url": "/production/static/media/train-station.710c0758.mp3"
+  },
+  {
+    "revision": "ccae070b2474c91159e3fccfa7e52322",
+    "url": "/production/static/media/train.ccae070b.mp3"
+  },
+  {
+    "revision": "fecd7bdc562707f69e01f027d909b295",
+    "url": "/production/static/media/tropical-waterfall.fecd7bdc.mp3"
+  },
+  {
+    "revision": "fd66b20d85d7ac859c8e1bf997f0d4dd",
+    "url": "/production/static/media/type-writer.fd66b20d.mp3"
+  },
+  {
+    "revision": "677f42b49cfb15f7888ffdcf6e12a4ec",
+    "url": "/production/static/media/underwater.677f42b4.mp3"
+  },
+  {
+    "revision": "bfdc3132b4f8d19beebc386173669515",
+    "url": "/production/static/media/washing-machine.bfdc3132.mp3"
+  },
+  {
+    "revision": "2fe3d821471696aa8b1bff4aac5593bd",
+    "url": "/production/static/media/waterfall.2fe3d821.mp3"
+  },
+  {
+    "revision": "5f778925dc8ceccfbf0fb8dfa27d10bb",
+    "url": "/production/static/media/waves-at-pier.5f778925.mp3"
+  },
+  {
+    "revision": "56d97c2ea09e1ec35bd0ff98621fda21",
+    "url": "/production/static/media/waves.56d97c2e.mp3"
+  },
+  {
+    "revision": "f635bcdd7f6114db6b0530b5f7e91a7a",
+    "url": "/production/static/media/whales.f635bcdd.mp3"
+  },
+  {
+    "revision": "69269b12245e468a55fcb7d501663841",
+    "url": "/production/static/media/white-board.69269b12.mp3"
+  },
+  {
+    "revision": "31fef8153245b43bfa4b33d425e22485",
+    "url": "/production/static/media/white-noise.31fef815.mp3"
+  },
+  {
+    "revision": "297a3e2b2e7fb99a2121c542c0305e86",
+    "url": "/production/static/media/wind-at-sea.297a3e2b.mp3"
+  },
+  {
+    "revision": "4ce96c95d79453af3e8809a35e05dd14",
+    "url": "/production/static/media/wind-chimes.4ce96c95.mp3"
+  },
+  {
+    "revision": "a7828ad2f7787cb70efb944024554cf1",
+    "url": "/production/static/media/wind-chimes2.a7828ad2.mp3"
+  },
+  {
+    "revision": "b4d4f58773a0ed80c6bd9bf74eb5c5fb",
+    "url": "/production/static/media/wind-chimes3.b4d4f587.mp3"
+  },
+  {
+    "revision": "887a160c710432ea9127aeeeb6891f5c",
+    "url": "/production/static/media/woodpecker.887a160c.mp3"
+  }
+]);
